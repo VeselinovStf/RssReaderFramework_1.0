4908f4b05a24ff41191c5ce955510300adac14cc
VeselinovStf <veselinovStf@gmail.com>
2019-05-06 07:49:08 +0300

    Adding dateTime wrapper

---
+++ src/RRF.DateTimeWrapper.Abstract/IDateTimePars.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+
+namespace RRF.DateTimeWrapper.Abstract
+{
+    public interface IDateTimePars
+    {
+        DateTime Parse(string elementDateTime, string format, CultureInfo cultureInfo);
+    }
+}
---
+++ src/RRF.DateTimeWrapper/DateTimeWrapper.cs
@@ -0,0 +1,14 @@
+﻿using RRF.DateTimeWrapper.Abstract;
+using System;
+using System.Globalization;
+
+namespace RRF.DateTimeWrapper
+{
+    public class DateTimeWrapper : IDateTimePars
+    {
+        public DateTime Parse(string elementDateTime, string format, CultureInfo cultureInfo)
+        {
+            return DateTime.ParseExact(elementDateTime, format, cultureInfo);
+        }
+    }
+}
