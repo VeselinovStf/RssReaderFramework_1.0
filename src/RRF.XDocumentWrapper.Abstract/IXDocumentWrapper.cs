1f93c037119c006aadcbac3afe7b3056a2570f81
VeselinovStf <veselinovStf@gmail.com>
2019-05-06 07:55:44 +0300

    Adding xDocument wrapper - Parse

---
+++ src/RRF.XDocumentWrapper.Abstract/IXDocumentWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace RRF.XDocumentWrapper.Abstract
+{
+    public interface IXDocumentWrapper
+    {
+        XDocument Parse(string RSSData);
+    }
+}
---
+++ src/RRF.XDocumentWrapper/XDocumentWrapper.cs
@@ -0,0 +1,14 @@
+﻿using RRF.XDocumentWrapper.Abstract;
+using System;
+using System.Xml.Linq;
+
+namespace RRF.XDocumentWrapper
+{
+    public class XDocumentWrapper : IXDocumentWrapper
+    {
+        public XDocument Parse(string RSSData)
+        {
+            return XDocument.Parse(RSSData);
+        }
+    }
+}
